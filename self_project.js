99114fcd32744b7028b1978e70f5e5d9601b513c
Mohamed Gamal <gmohamed777@yahoo.com>
2021-07-09 18:07:36 +0200

    creatproject

---
+++ self_project.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function()
+{
+$("#link3").click(function(){
+    $("#bton1").show()
+});
+});
+
+$(document).ready(function()
+                 {
+   $("#bton1").click(function()
+                 {
+       $("#text_box").slideToggle()
+   });
+});
+
